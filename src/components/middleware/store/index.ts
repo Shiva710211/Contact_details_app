30ff2b95a237562d1d4c1c41f3796b6f155f1f25
Shiva710211 <shivanandmuddi580@gmail.com>
2023-07-28 17:32:24 +0530

    first commit

---
+++ src/components/middleware/store/index.ts
@@ -0,0 +1,103 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { showToastMessage } from "../../utils/helpers";
+
+export interface Contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  status: string;
+}
+
+// interface RootState {
+//   todos: Contact[];
+// }
+
+interface ContactState {
+  items: Contact[];
+}
+
+const initialState: ContactState = {
+  items: [],
+};
+
+const contactSlice: any = createSlice({
+  name: "contacts",
+  initialState,
+  reducers: {
+    addContact: (state, action: PayloadAction<Contact>) => {
+      state.items.push(action.payload);
+      showToastMessage("Contact created.", "success");
+    },
+    removeContact: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter((todo) => todo.id !== action.payload);
+      showToastMessage("Contact deleted.", "error");
+    },
+    updateContact: (state, action: PayloadAction<Contact>) => {
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.items[index] = action.payload;
+        console.log(state.items[index]);
+      }
+      showToastMessage("Contact details updated.", "success");
+    },
+  },
+});
+
+export const { addContact, removeContact, updateContact } =
+  contactSlice.actions;
+
+export default contactSlice.reducers;
+
+const rootReducer = combineReducers({
+  contacts: contactSlice.reducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: {
+    contacts: {
+      items: [
+        {
+          id: 1,
+          firstName: "Vijay",
+          lastName: "V",
+          status: "Inactive",
+        },
+        {
+          id: 2,
+          firstName: "Rohit",
+          lastName: "Patil",
+          status: "Inactive",
+        },
+        {
+          id: 3,
+          firstName: "Robert",
+          lastName: "Open",
+          status: "Active",
+        },
+        {
+          id: 4,
+          firstName: "Surya",
+          lastName: "Yadav",
+          status: "Inactive",
+        },
+        {
+          id: 5,
+          firstName: "Yuraj",
+          lastName: "S",
+          status: "Active",
+        },
+        {
+          id:6,
+          firstName:"Raju",
+          lastName:"patil",
+          status:"Inactive",
+        },
+      ],
+    },
+  },
+});
